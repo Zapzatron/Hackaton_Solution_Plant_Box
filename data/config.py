8d11770386af0cbbec9e2d72b620adbe97af0162
Alexander Klochay <100136305+Zapzatron@users.noreply.github.com>
2023-06-03 15:43:46 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,3 +1,3 @@
-telegram_token = "6101137218:AAGBN-6m0CSmsvI07YdAIp3o6sTmoSoYoZM"
-mindsdb_login = "degget6564business@gmail.com"
-mindsdb_password = "As.whv6gXiJp8qL"
+telegram_token = "..."
+mindsdb_login = "..."
+mindsdb_password = "..."
